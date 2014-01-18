03365492a93f1a24143e3c4aa4e99f0c6ecaef91
senthilsweb@gmail.com <Power@1234>
2014-01-18 21:30:55 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    tweet = require('./routes/tweets'),
+    processemail = require('./routes/email2tweet'),
+    fs = require('fs');
+
+var filename = __dirname + "/config.json";
+var config = JSON.parse (fs.readFileSync(filename,'utf8'));
+ 
+var app = express();
+console.log("process.env.PORT=" + process.env.PORT)
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) { res.send(200); }
+  else { next(); }
+};
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+     app.use(allowCrossDomain);
+    app.use(express.bodyParser());
+});
+
+app.get('/tweets', tweet.findAll);
+app.get('/tweets/:id', tweet.findById);
+app.get('/emailtotweet', processemail.emailToTweet);
+app.get('/start', processemail.startProcessEmail);
+app.get('/stop', processemail.stopProcessEmail);
+
+app.listen(config.webserver.port);
+
+
+
+
+
